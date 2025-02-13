7ae23911b5bbd96376183e92994d067bf4407035
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2025-02-13 16:51:12 +0530

    Updated build

--- openapi-generate.sh
+++ openapi-generate.sh
@@ -4,4 +4,4 @@
 
 openapi-generator-cli generate -i ../schemaless-backend/src/build/swagger.json -g dart-dio -o . -c openapi-config.json &&
 
-cd schemaless_openapi && dart run build_runner build --delete-conflicting-outputs
+dart run build_runner build --delete-conflicting-outputs
